6109330c7d159561325acd5f9a78fce636497e31
eslavco <e.slavco@gmail.com>
2022-12-06 20:13:07 +0100

    Updated function to  short encoding

--- decoder.py
+++ decoder.py
@@ -1,28 +1,61 @@
-import sys
 import os
+import sys
+
+
+#  3F in hex
+ERROR_BYTE = 63
 
 
 def trivial_encoding(result_bytestring):
     return bytes([v for b in result_bytestring for v in (0, b)])
 
 
+def sublist(list_a, list_b):
+    if 0 == len(list_a):
+        return 0
+
+    if len(list_b) < len(list_a):
+        return -1
+
+    idx = -1
+    while list_a[0] in list_b[idx + 1:]:
+        idx = list_b.index(list_a[0], idx + 1)
+        if list_a == list_b[idx:idx + len(list_a)]:
+            return idx
+
+    return -1
+
+
 def short_encoding(result_bytestring):
     short_result = []
     mapped_bytes = []
-    for i in range(len(result_bytestring)):
+    i = 0
+    # iterate over each byte of result
+    while i < len(result_bytestring):
         b = result_bytestring[i]
-        # first check if I mapped the byte already
+        # first check if the byte was mapped
         if not b in mapped_bytes:
             mapped_bytes.append(result_bytestring[i])
-            short_result.append((0,b))
+            short_result.extend((0, b))
+            i += 1
         else:
-            for j in range(result_bytestring[i:]):
-                if result_bytestring[i:]:
+            # check the mapped list and
+            # get the index of longest sublist
+            length_sublist = 0
+            index_sublist = 0
+            for j in range(len(result_bytestring[i:])):
 
-            #       TODO:   function is not finished
-            #       we need to get the longest sublist from the mapped_bytes
-            #       and return the index
-                    pass
+                index = sublist(result_bytestring[i: i + j + 1], mapped_bytes)
+                if index >= 0:
+                    length_sublist = j + 1
+                    index_sublist = index
+                else:
+                    break
+
+            short_result.extend([len(mapped_bytes) - index_sublist, length_sublist])
+            mapped_bytes.extend(result_bytestring[i:length_sublist + i])
+            i += length_sublist
+    return short_result
 
 
 def decode(byte_stream):
@@ -37,9 +70,8 @@ def decode(byte_stream):
                 result_bytestring.extend(result_bytestring[-pi:][:qi])
         except:
             # Append 3F byte instead that correspond to int 63
-            result_bytestring.append(63)
-    return result_bytestring
-
+            result_bytestring.append(ERROR_BYTE)
+    return bytes(result_bytestring)
 
 
 def main():
@@ -48,17 +80,18 @@ def main():
     result_bytestring = decode(byte_stream)
     print(f" The corresponding string is {bytes(result_bytestring)}")
 
-    # Check if we need output the standard error
-    if 63 in result_bytestring:
+    # Assume that standard error needs to be show
+    # only if binary data has an invalid or incomplete pairs
 
+    if ERROR_BYTE in result_bytestring:
         env_variable = os.environ.get('USE_TRIVIAL_IMPLEMENTATION')
 
+        short_encoding(result_bytestring)
         if not env_variable:
             # trivial encoding
             print(trivial_encoding(result_bytestring))
         else:
-           print(short_encoding(result_bytestring))
-
+            print(short_encoding(result_bytestring))
 
 
 if __name__ == '__main__':
